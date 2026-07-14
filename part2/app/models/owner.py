61bf91b0867f746ae7a2f052134016cb41295b72
Mayamner <mem.kpope@gmail.com>
2026-07-14 18:27:01 +0300

    Adding the owner file

---
+++ part2/app/models/owner.py
@@ -0,0 +1,14 @@
+from app.models.user import User
+
+
+class Owner(User):
+    def __init__(self, first_name, last_name, email, password):
+        super().__init__(first_name, last_name, email, password)
+        self.places = []
+
+    def add_place(self, place):
+        place.owner = self
+        self.places.append(place)
+
+    def list_places(self):
+        return self.places

98a62851d7528f501372c166ca044604750b7e88
Mayamner <mem.kpope@gmail.com>
2026-07-14 18:15:07 +0300

    addd user

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,21 +1,17 @@
 from app.models.base_model import BaseModel
 
+
 class User(BaseModel):
-    users = []
     def __init__(self, first_name, last_name, email, password):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.passwprd = password
+        self.password = password
 
     def register(self):
-        User.users.append(self)
+        type(self)._store().append(self)
         return self
-    
+
     def update_profile(self, data):
         self.update(data)
-    
-    def delete(self):
-        if self in User.users:
-            User.users.remove(self)

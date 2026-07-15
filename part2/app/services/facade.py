3e21049a6260f54c5aac61d04d459f63606b5bfa
wahabxvii <auwa.mm@gmail.com>
2026-07-15 11:04:55 +0300

    add: add amenity methods to facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,3 +1,4 @@
+from app.models.amenity import Amenity
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
@@ -29,6 +30,27 @@ class HBnBFacade:
         user.update(user_data)
         return user
 
+
+    # ---------------- Amenity ----------------
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        amenity.update(amenity_data)
+        return amenity
+
+    # ---------------- Place ----------------
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
         # Logic will be implemented in later tasks

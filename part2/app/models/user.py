31ac682fb32409ece798f2a66e3cc6e355346ab5
wahabxvii <auwa.mm@gmail.com>
2026-07-19 16:29:32 +0300

    fix: email validation

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,5 +1,4 @@
 from app.models.base_model import BaseModel
-from app.utils.validators import validate_email
 
 
 class User(BaseModel):
@@ -7,8 +6,6 @@ class User(BaseModel):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
-        if not validate_email(email):
-            raise ValueError(f"Invalid email format: {email}")
         self.email = email
         self.password = password
 

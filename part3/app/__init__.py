c9acdeb578a01c30c13d58c5f88beaade33d6bed
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-07-28 18:01:22 +0300

    new branch - task 2

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -7,7 +7,9 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
+jwt = JWTManager()
 bcrypt = Bcrypt()
 
 
@@ -17,6 +19,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(owner_ns, path='/api/v1/owner')

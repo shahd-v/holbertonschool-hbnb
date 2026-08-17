539a0b7c6dc35e80eb4933731af400af54de9adf
Maya Muneer <mem.kpope@gmail.com>
2026-08-17 23:20:02 +0300

    fixing a login backend error

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,6 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -14,8 +15,12 @@ bcrypt = Bcrypt()
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
+    CORS(app, resources={r"/api/*": {"origins": "*"}}) 
+
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
+    
 
     authorizations = {
         'Bearer': {
